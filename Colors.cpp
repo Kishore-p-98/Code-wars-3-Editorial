44c601c974ef3615cd2ca8343ee8df84856ae02b
P Kishore <42121505+Kishore-p-98@users.noreply.github.com>
2019-02-27 18:52:09 +0530

    Added color solution

---
+++ Colors.cpp
@@ -0,0 +1,26 @@
+Colors : https://www.hackerrank.com/contests/codewars-3-rvce/challenges/colors-1
+
+The problem reduces to find the value of maximum of frequency of elements in the given array.
+
+CODE :
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+ int n,i,a;
+ cin>>n;
+
+ int freq[101]={0},ans=0;
+
+ for(i=0;i<n;i++)
+  {
+   cin>>a;
+   freq[a]++;
+   ans=max(ans,freq[a]);
+  }
+
+  cout<<ans<<endl;
+  return 0;
+}
